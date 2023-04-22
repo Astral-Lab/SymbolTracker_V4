5f35260b41599cda57cb52f6049035241531c4ba
Josh Brey <spacexplorer4@outlook.com>
2023-04-22 21:15:12 +0100

    changed browser router to hash router, changed homepage url

--- src/components/SymbolLocation.js
+++ src/components/SymbolLocation.js
@@ -3,8 +3,6 @@ import NavButton from "./NavButton";
 const SymbolLocation = ({image, iframe, description}) => {
     const selected = JSON.parse(localStorage.getItem("selectedSymbols"));
     const path = window.location.pathname;
-    console.log(path);
-    console.log(selected)
     const backPath = selected.current > 0 ? selected.paths[selected.current - 1] : `${path.slice(0, path.lastIndexOf('/'))}select`;
     const nextPath = selected.current < selected.paths.length - 1 ? selected.paths[selected.current + 1] : "/";
     const isLast = selected.current === selected.paths.length - 1;
@@ -12,8 +10,6 @@ const SymbolLocation = ({image, iframe, description}) => {
     // useEffect(() => {
     //     // put side effect in here instead, no need to block rendering
     // }, [])
-
-    console.log("SymbolLocation Re-render triggered!")
 
     return (
         <div className="grow flex flex-col justify-center items-center text-white text-2xl gap-4 lg:max-w-3xl">
