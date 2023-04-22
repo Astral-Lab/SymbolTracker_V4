29c3347804eb8bfd16aa3686bf7c2ae95f2a6c7a
Josh Brey <spacexplorer4@outlook.com>
2023-04-22 19:35:43 +0100

    added basename prop to router to see if it fixes 404 load error

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="https://astral-lab.github.io/SymbolTracker_V3/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
